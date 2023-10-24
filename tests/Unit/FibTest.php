2ad9a242e004f85881f8b49ced8100ff3c4ac2d5
oginoshikibu <oginoshikibu@gmail.com>
2023-10-24 21:43:12 +0900

    UnitTest書いた

--- tests/Unit/FibTest.php
+++ tests/Unit/FibTest.php
@@ -4,13 +4,47 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+use App\Models\NumSeq;
+
 class FibTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
+    /** @test */
+    public function GiveOne(): void
     {
-        $this->assertTrue(true);
+        $this->assertSame(1, NumSeq::fib(1));
     }
+
+    public function GiveTwo(): void
+    {
+        $this->assertSame(1, NumSeq::fib(2));
+    }
+    public function GiveThree(): void
+    {
+        $this->assertSame(2, NumSeq::fib(3));
+    }
+
+    public function Givezero(): void
+    {
+        $this->expectException(RangeException::class);
+        NumSeq::fib(0);
+    }
+    
+    public function GiveMinusOne(): void
+    {
+        $this->expectException(RangeException::class);
+        NumSeq::fib(-1);
+    }
+
+    public function GiveMinusTen(): void
+    {
+        $this->expectException(RangeException::class);
+        NumSeq::fib(-10);
+    }
+    
+    public function GiveStrings(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        NumSeq::fib("UnitTest");
+    }
+    
 }
