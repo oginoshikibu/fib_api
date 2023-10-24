d27a018b59437f228c6eaa2d84cf537815e8d683
oginoshikibu <oginoshikibu@gmail.com>
2023-10-24 22:29:59 +0900

    再帰関数での簡易的な実装

--- app/Models/NumSeq.php
+++ app/Models/NumSeq.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class NumSeq extends Model
 {
     use HasFactory;
+
+    public static function fib($num): int
+    {
+        if ($num==1 || $num==2){
+            return 1;
+        } else {
+            return NumSeq::fib($num-1) + NumSeq::fib($num-2);
+        }
+
+    }
 }
