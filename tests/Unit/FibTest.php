a46e85d4be6eda53af5fdf2329f92e2ca7061980
oginoshikibu <oginoshikibu@gmail.com>
2023-10-24 22:31:32 +0900

    アノテーション付け忘れ

--- tests/Unit/FibTest.php
+++ tests/Unit/FibTest.php
@@ -14,33 +14,40 @@ class FibTest extends TestCase
         $this->assertSame(1, NumSeq::fib(1));
     }
 
+    /** @test */
     public function GiveTwo(): void
     {
         $this->assertSame(1, NumSeq::fib(2));
     }
+
+    /** @test */
     public function GiveThree(): void
     {
         $this->assertSame(2, NumSeq::fib(3));
     }
 
+    /** @test */
     public function Givezero(): void
     {
         $this->expectException(RangeException::class);
         NumSeq::fib(0);
     }
     
+    /** @test */
     public function GiveMinusOne(): void
     {
         $this->expectException(RangeException::class);
         NumSeq::fib(-1);
     }
 
+    /** @test */
     public function GiveMinusTen(): void
     {
         $this->expectException(RangeException::class);
         NumSeq::fib(-10);
     }
     
+    /** @test */
     public function GiveStrings(): void
     {
         $this->expectException(InvalidArgumentException::class);
