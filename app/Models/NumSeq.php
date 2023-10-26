52e4b1df47edc6642159ecd09ebe3264bb2f120a
oginoshikibu <oginoshikibu@gmail.com>
2023-10-26 13:21:39 +0900

    style: spaceとrenam

--- app/Models/NumSeq.php
+++ app/Models/NumSeq.php
@@ -12,15 +12,15 @@ class NumSeq extends Model
 {
     use HasFactory;
 
-    public static function fib($num): int
+    public static function fib(int $index): string
     {
-        if (!is_int($num)){
+        if (!is_int($index)){
             throw new InvalidArgumentException("Not an integer");
         }
-        if ($num<1){
+        if ($index < 1){
             throw new RangeException("Not a positive integer");
         }
-        if ($num==1 || $num==2){
+        if ($index === 1 || $index === 2){
             return 1;
         } else {
             $a_1    = 1;
