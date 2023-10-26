84c88c9bb3db884408118bc62c990a1556c47e7c
oginoshikibu <oginoshikibu@gmail.com>
2023-10-26 14:47:45 +0900

    巨大な数のtestを追加

--- tests/Unit/FibTest.php
+++ tests/Unit/FibTest.php
@@ -33,7 +33,11 @@ class FibTest extends TestCase
     {
         $this->assertSame("218922995834555169026", NumSeq::fib(99));
     }
-
+    /** @test */
+    public function ok_soLargeIndex_returnNotOverFlow(): void
+    {
+        $this->assertSame("1262027241743996257169366534803711153432873792011637768873717598849301425880152551659880282149947993889708136584785538962348100239436771893992147449837835103812540911951967569050060912009607003831549523998076513", NumSeq::fib(1007));
+    }
     /** @test */
     public function ng_zeroIndex_returnRangeException(): void
     {
